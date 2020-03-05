a1e202e49671f8e56ca0f16d9047a7c5c8da8d13
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2020-03-04 20:45:27 -0600

    Final Frontend Functionality

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import HomePage from './pages/HomePage.js';
 import About from './pages/About-Us.js';
 import Article from './pages/ArticlePage.js';
 import Blog from './pages/Blog.js';
 import ArticleListPage from './pages/ArticleListPage';
 import NavBar from './pages/NavBar.js';
+import notFoundPage from './pages/NotFoundPage';
 
 
 
@@ -16,13 +17,14 @@ function App() {
       <Router>
          <NavBar />
          <div id = "page-body">
+        <Switch>
         <Route path ="/" component = {HomePage} exact />
         <Route path ="/about" component = {About} />
         <Route path ="/article/:name" component = {Article} />
         <Route path ="/article-list" component = {ArticleListPage} />
         <Route path ="/blog" component = {Blog} />
-        
-        
+        <Route component = {notFoundPage} />
+        </Switch>
         </div>
       </Router>
 
--- recipe-app/src/pages/ArticlePage.js
+++ recipe-app/src/pages/ArticlePage.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import articleContent from './article-content.js';
+import ArticleList from '../component/ArticleList.js';
+import notFoundPage from './NotFoundPage'
 
 const Article = ({match})=> {
     // Get the parameter from the URL
     const name = match.params.name;
-    const singleArticle = articleContent.find(article =>article.name === name)
-    if(!singleArticle) return <h1>Oh Sorry. No Article is found.</h1>   
+    const singleArticle = articleContent.find(article =>article.name === name);
+    const OtherArticle = articleContent.filter(article => (article.name !== name));
+    if(!singleArticle) return <notFoundPage />
+   
     return (
     <React.Fragment>
         <h1>{singleArticle.title}</h1>
      {singleArticle.content.map((paragraph, key) =>
  
      <p key ={key}>{paragraph}</p>
-    
+  
     )}
+    <h2>Related Articles</h2>
+  <ArticleList articles ={OtherArticle} />
     </React.Fragment>
 )
 }
